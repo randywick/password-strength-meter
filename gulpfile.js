5787a8afbfa36e444bd12a5303b426ab87496c52
Randy Wick <rwick@Randys-MacBook-Pro.local>
2016-03-08 16:22:21 -0600

    update

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+'use strict';
+
+
+const del = require('del');
+const es = require('event-stream');
+const path = require('path');
+const glob = require('glob');
+const browserify = require('browserify');
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const $ = require('gulp-load-plugins')();
+
+
+const DIST = 'dist';
+const dist = function(subpath) {
+  return !subpath ? DIST : path.join(DIST, subpath);
+};
+
+
+gulp.task('clean', done => {
+  del.sync([dist()])
+  done();
+})
+
+
+gulp.task('browserify', ['clean'], done => {
+
+  // map function for browserify and friends
+  const browserifyMapper = entry => {
+    const transform = [['babelify', {presets: ['es2015']}]]
+    const entries = [entry];
+    const b = browserify({entries, transform})
+
+    return b.bundle()
+      .pipe(source(entry))
+      .pipe(buffer())
+      .pipe($.sourcemaps.init({loadMaps: true}))
+        .pipe($.uglify())
+        .on('error', $.util.log)
+      .pipe($.sourcemaps.write('./'))
+      .pipe($.rename({
+        extname: '.bundle.js',
+        dirname: ''
+      }))
+      .pipe(gulp.dest(dist()))
+  }
+
+  glob(`./lib/index.js`, (err, files) => {
+    if (err) {
+      done(err);
+    }
+
+    const tasks = files.map(browserifyMapper);
+    es.merge(tasks).on('end', done);
+  })
+})
+
+
+gulp.task('js-watch', ['browserify'], browserSync.reload);
+
+
+gulp.task('serve-test', ['browserify'], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch(['lib/*.js', 'index.html'], ['js-watch']);
+});
+
+
+
+gulp.task('default', [
+  'clean',
+  'browserify'
+])
---
+++ tests/integration.js
@@ -0,0 +1,5 @@
+const Meter = require('../lib/index.js');
+
+
+const result = Meter.test('elatedboat242');
+console.log(result);
